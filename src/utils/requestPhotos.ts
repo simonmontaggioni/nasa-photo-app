e896e3e5f1f8818a3c2fe86cb1ad6b528270cbb6
simon.montaggioni <simon.montaggioni@gmail.com>
2023-08-13 01:13:44 -0400

    fix: Fixes type error for date on requestPhotos

--- src/utils/requestPhotos.ts
+++ src/utils/requestPhotos.ts
@@ -1,16 +1,16 @@
 import { RequestPhotosParams } from "@/interfaces/mainInterfaces";
+import { Dayjs } from "dayjs";
 
 export const requestPhotos = async (params: RequestPhotosParams) => {
-  const { rover, camera, dateType, date, page } = params;
+  const { rover, camera, dateType, page } = params;
   const searchParams = new URLSearchParams("");
   searchParams.append("rover", rover);
   searchParams.append("camera", camera);
   if (dateType === "earth") {
-    searchParams.append(
-      "earth_date",
-      date?.format("YYYY-MM-DD").toString() ?? ""
-    );
+    const date: Dayjs = params.date as Dayjs;
+    searchParams.append("earth_date", date?.format("YYYY-MM-DD") ?? "");
   } else {
+    const date: number = params.date as number;
     searchParams.append("sol", String(date));
   }
 
